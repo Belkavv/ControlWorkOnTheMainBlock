e834ee7f3cae26b3726da56d8cab802699c0fa90
Belkavv <belkax111@mail.ru>
2023-08-12 18:29:31 +0300

    added Task

---
+++ Task/Program.cs
@@ -0,0 +1,34 @@
+﻿String[] TreeSimvolArray(String[] array)
+{
+    String[] resultArray = new String[array.Length];
+    int j = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            resultArray[j] = array[i];
+            j++;
+        }
+    }
+    return resultArray;
+}
+
+void PrintArray(string[] array)
+{
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+String[] array_1 = new String[] {"Hello", "2", "world", ":-)"};
+String[] array_2 = new String[] {"1234", "1567", "-2", "computer science"};
+String[] array_3 = new String[] {"Russia", "Denmark", "Kazan"};
+
+PrintArray(TreeSimvolArray(array_1));
+PrintArray(TreeSimvolArray(array_2));
+PrintArray(TreeSimvolArray(array_3));
